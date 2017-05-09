066da7b30cb9d52d4e60529a9e66b114c17d8735
manujua82 <manujua@gmail.com>
2017-05-09 17:38:54 -0400

    Initial Commit

---
+++ SlidebarMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlidebarMenu
+//
+//  Created by Juan Salcedo on 5/9/17.
+//  Copyright © 2017 Juan Salcedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
