6acdbdd9d25e64a2e6d8903a3253394f96e2726e
manujua82 <manujua@gmail.com>
2017-05-09 19:34:31 -0400

    Add Menu Optios

--- SlidebarMenu/ViewController.swift
+++ SlidebarMenu/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.addSlideMenuButton()
     }
 
     override func didReceiveMemoryWarning() {
